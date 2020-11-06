e85a998668533044ccfc90a4a6c08531a9815b7e
DESKTOP-E2HJ0GG\Alalamiya <mohammed.irheem@student.kyh.se>
2020-11-06 10:20:36 +0100

    Skapar Klass nod

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,76 +1,42 @@
-
 public class LinkedList {
- //        add(e) -> Lägger till element e till slutet av listan
-//        add(i, e) -> Lägger till element e till position/index i, element till "höger" om i flyttar ett steg till höger
-//        contains(e) -> true om e finns i listan annars false
-//        get(i) -> returnerar elementet på position/index i
-//        remove(i) -> ta bort elementet på position/index i
-//        empty() -> true om listan är tom, annars false
-//        size() -> antalet element i listan
 
-    
-        private Node head;
+    Nod head=null;
+    Nod tail=null;
+    int size = 0;
 
-        public int size(int i){
-            if (head == null){
-                return 0;
-            }
-            else {
-                i = 0;
-                Node n = head;
-                while (n.next !=null){
-                    n = n.next;
-                    i++;
-                } return i;
-            }
 
-        }
+    //Här Skapat  Objekt från klass Nod som har value OCH NEXT.void utan reture för att jag vill bara att adda element
+// Ta hansyn till att det här är det första element så innehåller det (head och tail).
+    public void Add_last (int element){
+        if (Nod == null){
+            Nod e = new Nod();
+            e.value=element;
+            head=e;
+            tail=e;
+            size++;
 
-        public void add(int e ){
-            if (head == null){
-                head = new Node (e);
-            }
-            else {
-                Node n = head;
-                while (n.next != null){
-                    n = n.next;
-                }
-                n.next = new Node(e);
-            }
-        }
-        public void add(int index, int value){
-            get (index);
-            add (value);
+        } else {
+            Nod e= new Nod() ;
+            e.value= element ;
+            //Om jag vill tillägga en element sist så måste det lika med tail .Eftersom tail alltid finns på sist.
+            tail.next = e ;
+            tail= e ;
+            size++ ;
+
 
 
         }
-        public int contains(int e ){
-
-            return e;
-        }
-        public int get(int index ){
-            int i = 0;
-            Node n = head;
-            while (i < index){
-                n = n.next;
-                i++;
-            }
-            return n.value;
-        }
-
-        public void remove(int i ){
+        public void Add2 (char e ,int index ){
+            Nod e=new Nod();
+            e.value = element ;
+            e.next=e;
+            head= e ;
+            size++ ;
 
         }
-        public void empty(){
-
-        }
-
-        static class Node{
-            protected int value;
-            protected Node next;
-
-            public Node(int e){
-                this.value = e;
-            }
-        }
+        public void remove ( Nod i )
+        Nod i = head ;
+        while (h.)
     }
+
+}
