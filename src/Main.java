106b4c815a1175b93c884d8d4d61d30c68738c58
Ihisan <ihisan.bashe@student.kyh.se>
2020-11-06 10:24:23 +0100

    add

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        LinkedList funktion = new LinkedList ();
-        //================
-        funktion.add (0);
-        funktion.add (1,0);
+        LinkedList list = new LinkedList ();
+        //================Funktioner=======
+        list = insert
 
 
-
-        //===============
-        System.out.println (funktion.get (0));
-        System.out.println (funktion.size (1));
-        funktion.add(0,10);
+        //===============Skriver ut======
     }
 }
