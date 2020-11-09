54cba1e031fdba95c928f18cad5368455e2ed342
Ihisan <ihisan.bashe@student.kyh.se>
2020-11-09 13:31:25 +0100

    Rensat

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,15 +1,7 @@
-
-//        add(e) -> Lägger till element e till slutet av listan
-//        add(i, e) -> Lägger till element e till position/index i, element till "höger" om i flyttar ett steg till höger
-//        contains(e) -> true om e finns i listan annars false
-//        get(i) -> returnerar elementet på position/index i
-//        remove(i) -> ta bort elementet på position/index i
-//        empty() -> true om listan är tom, annars false
-//        size() -> antalet element i listan
-
 public class LinkedList {
 
     private Node head;
+    private Node tail;
 
     public int size(int index) {
         if (head == null) {
@@ -47,56 +39,42 @@ public class LinkedList {
             n.next = new Node (element);
         }
     }
-        public void add(int index, int value){
-           Node node = new Node (value);
-           if (index == 0){
-               node.next = head;
-               head = node;
-           } else{
-               int i = 0;
-               Node n = head;
-               while(i < index -1){
-                   n = n.next;
-                   i++;
-
-               }
-               node.next = n.next;
-               n.next = node;
-
-           }
-        }
-        public void prettyPrint(){
-            System.out.print("Linkedlist -> ");
+    public void add(int index, int value){
+        Node node = new Node (value);
+        if (index == 0){
+            node.next = head;
+            head = node;
+        } else{
+            int i = 0;
             Node n = head;
-            while(n != null) {
-                System.out.print ("Node ("+n.value+")->");
+            while(i < index -1){
                 n = n.next;
+                i++;
+
             }
+            node.next = n.next;
+            n.next = node;
+
         }
+    }
+    public void prettyPrint(){
+        System.out.print("Linkedlist -> ");
+        Node n = head;
+        while(n != null) {
+            System.out.print ("Node ("+n.value+")->");
+            n = n.next;
+        }
+    }
 
-   public boolean empty() {
+    public boolean empty() {
         return head == null;
-   }
+    }
 
-  /* public void remove(int index){
-       Node node = new Node (head.value);
-       if (index == 0){
-           node.next = head;
-           head = node;
-       } else{
-           int i = 0;
-           Node n = head;
-           while(i < index -1){
-               n = n.next;
-               i++;
-
-           }
-           node.next = n.next;
-           n.next = node;
-
-       }
-   }*/
-   public void remove(int index) {
+    public int pop(){
+        remove (0);
+        return get (0);
+    }
+    public int remove(int index) {
         Node previouse = head;
         int count = 1;
         while (get (0) < head.value - 1) {
@@ -108,9 +86,11 @@ public class LinkedList {
             previouse.next = current.next;
             current.next = null;
         }
+
+        return count;
     }
 
-    public void contains(int index) {
+    public void contains() {
         Node node = new Node (head.value);
         while (node.next != null) {
             System.out.println ("It contains: " + node.value + true);
@@ -120,10 +100,31 @@ public class LinkedList {
     }
 
 
+    public void push(int value) {
+        add (0,value);
+
+
+    }
+
+    public int peek(){
+
+        return get (0);
+    }
+
+    public void prettyPrint2(){
+        System.out.print("\nLinkedlist After Push,Pop,Peek -> ");
+        Node n = head;
+        while(n != null) {
+            System.out.print ("Node ("+n.value+")->");
+            n = n.next;
+        }
+    }
+
 
     class Node {
         protected int value;
         protected Node next;
+        protected Node tail;
 
         public Node(int v) {
             this.value = v;
